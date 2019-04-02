23eef8655ad0bf3b601e3228f8c664d1274b67e4
Cristian Restrepo <crestrepo21@gmail.com>
2019-04-01 17:37:07 -0700

    randomizer per hr done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
-var hours = [0600, 0700, 0800, 0900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000];
-var cutomers = {
+'use strict'
+
+var hours = [600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000];
+var customers = {
   store: '1st and Pike',
   minCustHr: 23,
   maxCustHr: 65,
   customerHr: [],
   avgCookieHr: 6.3,
   saleHr: [],
-  custRandomizer: function(min, max){
+  randomizer: function(){
     var min = Math.ceil(this.minCustHr);
     var max = Math.floor(this.maxCustHr);
     return Math.floor(Math.random()*(max - min + 1)) + min;
-  }
-  
+  },
+  hourRandomizer: function(){
+    for (var i = 0; i < hours.length; i++){
+      this.customerHr.push(this.randomizer());
+    }
+    console.log(this.customerHr);
+    return this.customerHr;
+  },
 };
-cutomers.custRandomizer();
 
-console.log(cutomers.custRandomizer());
+customers.randomizer();
+
+console.log(customers.randomizer());
+
+customers.hourRandomizer();
+
+console.log(customers.hourRandomizer());
