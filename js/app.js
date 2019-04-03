e227f1aeb6c5fb4cf32198ddd8fd4756f5afbbc0
Cristian Restrepo <crestrepo21@gmail.com>
2019-04-03 00:31:52 -0700

    everything but hourly total displayed correctly

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var hours = ['0600 hrs', '0700 hrs', '0800 hrs', '0900 hrs', '1000 hrs', '1100 hrs', '1200 hrs', '1300 hrs', '1400 hrs', '1500 hrs', '1600 hrs', '1700 hrs', '1800 hrs', '1900 hrs', '2000 hrs'];
+var hours = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
 var allCustomers = [];
 
-function Customers (storeName, minimumCustomer, maximumCustomer, cookieAvgPerHour){
+function Customers(storeName, minimumCustomer, maximumCustomer, cookieAvgPerHour) {
   this.store = storeName;
   this.minCustHr = minimumCustomer;
   this.maxCustHr = maximumCustomer;
@@ -22,20 +22,20 @@ new Customers('Alki', 2, 16, 4.6);
 
 
 // Picks a random number between min and max
-Customers.prototype.randomizer = function() {
+Customers.prototype.randomizer = function () {
   // line from MDN Math.random docs
   return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
 }
 
 // Inputs random number of customers into the Array at the given index
-Customers.prototype.hourRandomizer = function() {
+Customers.prototype.hourRandomizer = function () {
   for (let i = 0; i < hours.length; i++) {
     this.customerHr.push(this.randomizer(this.minCustHr, this.maxCustHr));
   }
 }
 
 // Calculates the number of cookies sold per hour based on the number of customers at given index. configured in class following the step of instructor Sam Hamm
-Customers.prototype.saleCalc= function() {
+Customers.prototype.saleCalc = function () {
   for (let i = 0; i < hours.length; i++) {
     var oneHour = Math.ceil(this.customerHr[i] * this.avgCookieHr);
     this.saleHr.push(oneHour);
@@ -46,29 +46,8 @@ Customers.prototype.saleCalc= function() {
 }
 var storeStatsTable = document.getElementById('storeStats');
 
-Customers.prototype.render = function() {
-  var trEl = document.createElement('tr');
-  // store location
-  var tdEl = document.createElement('td');
-  tdEl.textContent = this.store;
-  trEl.appendChild(tdEl);
 
-  for (var i = 0; i < hours.length; i++) {
-  // cookies sold content
-  tdEl = document.createElement('td');
-  tdEl.textContent = this.saleHr[i];
-  trEl.appendChild(tdEl);
-  }
-  // store total of cookies sold 
- tdEl = document.createElement('td');
- tdEl.textContent = this.saleTotal;
- trEl.appendChild(tdEl);
-//  projected cookies to keep on reserves
- tdEl = document.createElement('td');
- tdEl.textContent = cookieReserve[i];
- trEl.appendChild(tdEl);
- storeStatsTable.appendChild(trEl);
-};
+
 // Sums the total cookies for each store at a given hour
 var totalSalesPerHr = [];
 function salesPerHr() {
@@ -77,25 +56,81 @@ function salesPerHr() {
   }
   console.log(totalSalesPerHr);
   // The following line of code was from https://stackoverflow.com/questions/34458132/how-to-sum-elements-at-the-same-index-in-array-of-arrays-into-a-single-array
-  totalSalesPerHr = totalSalesPerHr.reduce((r, a) => a.map((b, i)=>(r[i] || 0) + b), []);
+  totalSalesPerHr = totalSalesPerHr.reduce((r, a) => a.map((b, i) => (r[i] || 0) + b), []);
   console.log(totalSalesPerHr);
 }
 // gets the avg number of cookies sold per hour, by location, then adds to the total for a projected cookies reserve dependant on customer traffic
 var cookieReserve = []
-function reserve(){
-  for (let i = 0; i < allCustomers.length; i++){
+function reserve() {
+  for (let i = 0; i < allCustomers.length; i++) {
     cookieReserve.push(Math.ceil((allCustomers[i].saleTotal / hours.length) + allCustomers[i].saleTotal))
   }
+  //  projected cookies to keep on reserves
+  // var trEl = document.createElement('tr');
+  // var tdEl = document.createElement('td');
+  // tdEl = document.createElement('td');
+  // tdEl.textContent = cookieReserve;
+  // trEl.appendChild(tdEl);
+  // storeStatsTable.appendChild(trEl);
   console.log(cookieReserve);
 }
+Customers.prototype.render = function () {
+  let trEl = document.createElement('tr');
+  // store location
+  let tdEl = document.createElement('td');
+  tdEl.textContent = this.store;
+  trEl.appendChild(tdEl);
+
+  for (var i = 0; i < hours.length; i++) {
+    // cookies sold content
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.saleHr[i];
+    trEl.appendChild(tdEl);
+  }
+  // store total of cookies sold 
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.saleTotal;
+  trEl.appendChild(tdEl);
+  storeStatsTable.appendChild(trEl);
+};
+function headerRow(){
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Location & Hours';
+  trEl.appendChild(thEl);
+  for (var i = 0; i < hours.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Location Total'
+  trEl.appendChild(thEl);
+  storeStatsTable.appendChild(trEl);
+}
+function footerRow(){
+  var trEl = document.createElement('tr');
+  var tfEl = document.createElement('tf');
+  tfEl.textContent = 'Total All Locations';
+  trEl.appendChild(tfEl);
+  for (var i = 0; i < totalSalesPerHr.length; i++){
+    tfEl = document.createElement('tf');
+    tfEl.textContent = totalSalesPerHr[i];
+    trEl.appendChild(tfEl);
+  }
+  storeStatsTable.appendChild(trEl);
+}
 // store selector
-function selector(){
-  for (let i = 0; i < allCustomers.length; i++){
+function selector() {
+  for (let i = 0; i < allCustomers.length; i++) {
     allCustomers[i].hourRandomizer();
     allCustomers[i].saleCalc();
     allCustomers[i].render();
-  };
+  }
 }
+headerRow();
+console.log(headerRow);
 selector(); //selects each store and runs all the functios
 salesPerHr(); //selects each index of all the sales array to get sale for the hour 
 reserve();
+footerRow();
